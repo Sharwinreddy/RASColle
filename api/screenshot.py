afa718cec6fdeda802e8b4c4a2547dfc559c5fa0
sharwin <psharwinreddy@gmail.com>
2021-01-13 14:55:18 -0500

    Added python file to take screenshot and integrated with allsub.py for testing.
    The output will be in a format

---
+++ api/screenshot.py
@@ -0,0 +1,19 @@
+import os,sys
+import csv
+
+projectnumber=str(sys.argv[2])
+projectpath='output/'+projectnumber+"/"
+csvpath=projectpath+str(sys.argv[1])+".csv"
+os.system('mkdir '+projectpath+'temp')
+with open(csvpath, newline='') as csvfile:
+    data = csv.DictReader(csvfile)
+    count=0
+    for row in data:
+        count=count+1
+        os.system('mkdir '+projectpath+str(count))
+        os.system('mkdir '+projectpath+str(count)+'/screenshots')
+        if row['Status']=='live':
+            open(projectpath+'turls.txt', 'w').write(str(row['Urls']))
+            os.system('eyewitness --web --timeout 60 -f '+projectpath+'turls.txt -d '+projectpath+'temp'+' --no-prompt')
+            os.system('mv -v '+projectpath+'temp/screens/* '+projectpath+str(count)+'/screenshots/')
+            os.remove(projectpath+'turls.txt')

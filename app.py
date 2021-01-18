f9474c4f3391ec171b50aeac2b30258526fb054a
sharwin <psharwinreddy@gmail.com>
2021-01-18 09:54:00 -0500

    Implimented Dasboard and DB

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import time
 from celery import Celery
 import os
 import csv
+import sqlite3
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top-secret!'
 #celery configuration
@@ -17,49 +18,41 @@ celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 @celery.task(bind=True)
-def runscan(self,domain,projectnumber):
+def runscan(self,domain):
+    connection = sqlite3.connect("small_db.db")
+    projectnumbersql = ''' select * from scans'''
+    cursor = connection.cursor()
+    cursor.execute(projectnumbersql)
+    projectnumber=len(cursor.fetchall())+1
+    projectnumber=str(projectnumber)
+    sqlstat= ''' INSERT INTO scans(Project_Number,Status,Domain)
+                      VALUES(?,?,?) '''
+    statusupdate=''' update scans set Status = ?  where Project_Number= ?'''
+    params=[projectnumber,'Started',domain]
+    cursor.execute(sqlstat,params)
+    connection.commit()
     self.update_state('started subdomainscan')
     os.system("cd api && python3 allSub.py "+str(domain)+" "+projectnumber)#running subdomain scan
-    self.update_state('finished subdomainscan')
-    return {'current': 100, 'total': 100, 'status': 'Task completed!',
-            'result': 42}
+    cursor.execute(statusupdate,['Sudomain Enumuration Done',projectnumber])
+    connection.commit()
+    self.update_state('started screenshots')
+    os.system("cd api && python3 screenshot.py "+str(domain)+" "+projectnumber)
+    cursor.execute(statusupdate,['alldone',projectnumber])
+    connection.commit()
+    self.update_state('all done')
+    return redirect(url_for('dashboard'))
 #running scan
-@app.route('/scan/<projectnumber>/<domain>', methods=['GET','POST'])
-def scan(domain,projectnumber):
-    task = runscan.apply_async(args=[str(domain),str(projectnumber)])
+@app.route('/scan', methods=['GET','POST'])
+def scan():
+    task = runscan.apply_async(args=[str(request.form['domain'])])
     print(str(task.id))
-    return redirect(url_for(scanstatus),task_id=str(task.id))
+    return redirect(url_for('dashboard'))
 
 #status update
 @app.route('/status/<task_id>')
 def scanstatus(task_id):
     task = runscan.AsyncResult(task_id)
-    if task.state == 'PENDING':
-        # job did not start yet
-        response = {
-            'state': task.state,
-            'current': 0,
-            'total': 1,
-            'status': 'Pending...'
-        }
-    elif task.state != 'FAILURE':
-        response = {
-            'state': task.state,
-            'current': task.info.get('current', 0),
-            'total': task.info.get('total', 1),
-            'status': task.info.get('status', '')
-        }
-        if 'result' in task.info:
-            response['result'] = task.info['result']
-    else:
-        # something went wrong in the background job
-        response = {
-            'state': task.state,
-            'current': 1,
-            'total': 1,
-            'status': str(task.info),  # this is the exception raised
-        }
-    return jsonify(response)
+    return task.state
 #getting data to populate the output
 
 #output function
@@ -85,6 +78,44 @@ def internal_error(e):
     return render_template('500.html'), 500
 app.register_error_handler(404, page_not_found)
 app.register_error_handler(500, internal_error)
+
+#getting Statistics
+def getstats():
+    connection = sqlite3.connect("small_db.db")
+    stast='''select * from scans'''
+    cursor = connection.cursor()
+    cursor.execute(stast)
+    list=[]
+    result = cursor.fetchall()
+    sub=0
+    lsub=0
+    '''
+    for r in result:
+        temp=[]
+        if r[1]!='':
+            sub=sub+int(r[1])
+            lsub=lsub+int(r[2])
+    connection.close()
+    '''
+    return [sub,lsub]
+#getscan details
+def getscans():
+    connection = sqlite3.connect("small_db.db")
+    scans='''select * from scans'''
+    cursor = connection.cursor()
+    cursor.execute(scans)
+    list=[]
+    result = cursor.fetchall()
+    for r in result:
+        list.append(r)
+    connection.close()
+    return list
+#dashboard rendering
+@app.route('/dashboard')
+def dashboard():
+    stats=getstats()
+    return render_template('dashboard.html',total=str(stats[0]),live=str(stats[1]),scans=getscans())
+
 
 if __name__ == '__main__':
    app.run(debug = True, ssl_context=('certs/cert.pem', 'certs/key.pem'))
